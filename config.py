e672ee904c14720048c1430e23ee144f6bab65b8
hyu9999 <757147959@qq.com>
2020-06-02 15:47:21 +0800

    ADD 增加日志配置

--- config.py
+++ config.py
@@ -1,33 +1,63 @@
+import logging
 import os
 
+
 from paper_trading.utility.constant import ConfigType
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "j1as78a1gf6a4ea1f5d6a78e41fa56e"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "j1as78a1gf6a4ea1f5d6a78e41fa56e"
 
     @staticmethod
     def init_app(app):
         pass
 
+
 class TradingConfig(Config):
-    MONGO_HOST = "192.168.1.254"
+    """本地"""
+    MONGO_HOST = "localhost"
     MONGO_PORT = 27017
     LOG_FILE_NAME = ""
     LOG_FILE_PATH = ""
     SERVER_HOST = "0.0.0.0"
     SERVER_PORT = 5000
+    LOG_FORMAT = {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(name)s][%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            }
+        },
+        "loggers": {
+            "flask.app": {"level": logging.WARNING},
+            "lazyTrader": {"level": logging.WARNING},
+            "werkzeug": {"level": logging.WARNING},
+            "apscheduler.scheduler": {"level": logging.WARNING},
+        },
+        "root": {"level": "INFO", "handlers": ["wsgi"]},
+    }
 
 
 class TestingConfig(Config):
+    """测试环境"""
+
     MONGO_HOST = "localhost"
     MONGO_PORT = 27017
     LOG_FILE_NAME = ""
     LOG_FILE_PATH = ""
     SERVER_HOST = "0.0.0.0"
     SERVER_PORT = 5001
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -43,5 +73,5 @@ config = {
     ConfigType.TESTING.value: TestingConfig,
     ConfigType.TRADING.value: TradingConfig,
     ConfigType.DEVELOPMENT.value: DevelopmentConfig,
-    ConfigType.DEFAULT.value: TradingConfig
+    ConfigType.DEFAULT.value: TradingConfig,
 }
